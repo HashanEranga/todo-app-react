5d5bb4ba298e26a7e4613bfe596dede49deef892
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2024-04-15 15:22:57 +0530

    Feat() : Added initial components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Todos from './Components/Todos/Todos';
+
+function App() {
+  return (
+    <>
+    <div>
+      <h1>Todo Application</h1>
+      <Todos/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Todos/Todos.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Todo from "../Todo/Todo"
+
+function Todos() {
+    return (
+        <>
+            <h1>Todos list</h1>
+            <Todo/>
+            <Todo/>
+            <Todo/>
+            <Todo/>
+        </>
+    )
+}
+
+export default Todos
